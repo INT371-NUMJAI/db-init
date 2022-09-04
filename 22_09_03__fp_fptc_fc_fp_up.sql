d0e6af99f65d35f0307c1ead38ad697e221edb37
Qwanjai <d_khonjai@hotmail.com>
2022-09-05 01:46:18 +0700

    add alter drop and add column

--- 22_09_03__fp_fptc_fc_fp_up.sql
+++ 22_09_03__fp_fptc_fc_fp_up.sql
@@ -1,3 +1,6 @@
+ALTER TABLE fdn_projects DROP COLUMN resource_uuid;
+ALTER TABLE fdn_projects ADD COLUMN resource_uuid VARCHAR(50) REFERENCES resources;
+
 delete from fdn_projects where fdn_project_uuid in ('43a047f7-55c2-49cc-9aa2-19527da04727', 'cef1b5a2-fc05-4405-8c1e-95ee06ea97ec', '62f9fb70-8ab0-4ae6-a1af-f55d76a88bad', 'a2ea259a-84c4-4aff-baf8-42a76368aadb', '7b17238a-8c41-42a9-8b9a-b6bc742fd0f4');
 
 insert into fdn_projects (fdn_project_uuid, fp_name, start_date, end_date, goal, create_date, fdn_uuid, resource_uuid)
@@ -60,6 +63,8 @@ values ('77ae5dd0-bf5a-408f-bd47-afe974687233', '5');
 insert into fdn_categories (fdn_uuid, category_id)
 values ('c2fcddbd-3640-418b-be2b-36c165f541b7', '9');
 
+ALTER TABLE fdn_posts DROP COLUMN resource_uuid;
+ALTER TABLE fdn_posts ADD COLUMN resource_uuid VARCHAR(50) REFERENCES resources;
 
 delete from fdn_posts where fdn_post_uuid in ('c2bbd3ec-8bcc-415c-b9c8-494b3703645c', '798d8c2d-d50a-4ef8-95cb-bee978ac4127', '35181b43-5be0-4bf6-b205-fe193ce4523b', '81ebf3e5-b29b-4311-b22f-81d937faf116', '1e68e3a0-247f-4fd1-b265-f4e05d50ea3c', '8dc866f8-bf4d-48c5-8aa3-36e2e5f9f39e');
 
@@ -81,6 +86,8 @@ values ('1e68e3a0-247f-4fd1-b265-f4e05d50ea3c', 'ลามะคอปเตอ�
 insert into fdn_posts (fdn_post_uuid, post_title, post_detail, create_date, resource_uuid, fdn_uuid)
 values ('8dc866f8-bf4d-48c5-8aa3-36e2e5f9f39e', 'แมมโบ้จุ๊ย แฮนด์ซาฟารีแฟกซ์รุสโซ แดนเซอร์บร็อกโคลีป๊อก', 'บว์คอนเซ็ปต์ หลวงปู่ไมค์โพสต์กิฟท์แรงผลัก แกรนด์แฟนตาซีอัลตรา พูลอิมพีเรียลแหววแม็กกาซีนมาร์ก คอนโดโก๊ะนิวแอคทีฟก๋ากั่น ตัวเองไวกิ้งแดนซ์ซัพพลายสุนทรีย์ ไชน่านาฏยศาลา', now(), null, 'c2fcddbd-3640-418b-be2b-36c165f541b7');
 
+ALTER TABLE user_posts DROP COLUMN resource_uuid;
+ALTER TABLE user_posts ADD COLUMN resource_uuid VARCHAR(50) REFERENCES resources;
 
 delete from user_posts where user_post_uuid in ('52e94ee8-7da2-4154-ba73-1aff4600fdd1', 'ae545ccb-c505-420d-8883-c52bf942c8a5');
 
