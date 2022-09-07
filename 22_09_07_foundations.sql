4fcf8b91d922cff2cd37abc8d74a884f3a525a5a
Qwanjai <d_khonjai@hotmail.com>
2022-09-07 20:05:58 +0700

    alter column : users

--- 22_09_07_foundations.sql
+++ 22_09_07_foundations.sql
@@ -1,2 +1,5 @@
 ALTER TABLE foundations DROP COLUMN create_date;
 ALTER TABLE foundations ADD COLUMN create_date DATE DEFAULT NOW();
+
+ALTER TABLE users DROP COLUMN create_date;
+ALTER TABLE users ADD COLUMN create_date DATE DEFAULT NOW();
