187a566b445f90c666bb6da7edf75b098310f7ed
Qwanjai <d_khonjai@hotmail.com>
2022-09-08 00:09:31 +0700

    alter column : fdn_rejected

--- 22_09_07_foundations.sql
+++ 22_09_07_foundations.sql
@@ -3,3 +3,6 @@ ALTER TABLE foundations ADD COLUMN create_date DATE DEFAULT NOW();
 
 ALTER TABLE users DROP COLUMN create_date;
 ALTER TABLE users ADD COLUMN create_date DATE DEFAULT NOW();
+
+ALTER TABLE fdn_rejected DROP COLUMN create_date;
+ALTER TABLE fdn_rejected ADD COLUMN create_date DATE DEFAULT NOW();
