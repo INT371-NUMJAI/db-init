be55a23376e22cff929eb2379f0d9c847d44684e
Qwanjai <d_khonjai@hotmail.com>
2022-09-10 02:36:24 +0700

    alter column : fdn_projects

--- 22_09_07_foundations.sql
+++ 22_09_07_foundations.sql
@@ -6,3 +6,6 @@ ALTER TABLE users ADD COLUMN create_date DATE DEFAULT NOW();
 
 ALTER TABLE fdn_rejected DROP COLUMN create_date;
 ALTER TABLE fdn_rejected ADD COLUMN create_date DATE DEFAULT NOW();
+
+ALTER TABLE fdn_projects DROP COLUMN resource_uuid;
+ALTER TABLE fdn_projects ADD COLUMN picture_path TEXT;
