e689cb6febbd476e41b4922ccdfc958ce4db3dcb
fxxhhhhhhh <77713539+fxxhhhhhhh@users.noreply.github.com>
2022-09-10 21:05:32 +0700

    update table: fun_projects

--- 22_09_07_foundations.sql
+++ 22_09_07_foundations.sql
@@ -18,3 +18,5 @@ UPDATE foundations SET status = 'VERIFIED' WHERE status = 'APPROVED';
 
 ALTER TABLE fdn_projects ADD COLUMN fp_detail TEXT;
 ALTER TABLE fdn_projects ADD COLUMN fp_detail_place TEXT;
+
+UPDATE fdn_projects SET fp_detail = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', fp_detail_place = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.' WHERE fp_detail IS null AND fp_detail_place IS null; 
