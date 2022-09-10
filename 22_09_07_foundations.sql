0363cd9e812b88014f39d782dfeb3488fca5d07d
fxxhhhhhhh <77713539+fxxhhhhhhh@users.noreply.github.com>
2022-09-11 00:42:02 +0700

    alter table, update table: fun_projects

--- 22_09_07_foundations.sql
+++ 22_09_07_foundations.sql
@@ -20,3 +20,6 @@ ALTER TABLE fdn_projects ADD COLUMN fp_detail TEXT;
 ALTER TABLE fdn_projects ADD COLUMN fp_detail_place TEXT;
 
 UPDATE fdn_projects SET fp_detail = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', fp_detail_place = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.' WHERE fp_detail IS null AND fp_detail_place IS null; 
+
+ALTER TABLE fdn_projects ADD COLUMN responsible_person VARCHAR(255);
+UPDATE fdn_projects SET responsible_person = 'John Smith' WHERE responsible_person IS NULL;
